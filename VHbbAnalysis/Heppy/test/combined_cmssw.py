c3fcedf6397cf878957dd193cc42820a394c5fbe
Joosep <joosep.pata@cern.ch>
2015-09-04 18:32:24 +0200

    switch to miniaod genjets

--- VHbbAnalysis/Heppy/test/combined_cmssw.py
+++ VHbbAnalysis/Heppy/test/combined_cmssw.py
@@ -382,15 +382,7 @@ for fatjet_name in ["ak08PFPrunedJetsCHS", "ca15PFPrunedJetsCHS", "looseOptRHTT"
 process.load("SimGeneral.HepPDTESSource.pythiapdt_cfi")
 
 genParticleCollection = 'prunedGenParticles'
-genJetInputParticleCollection = 'packedGenParticles'
-
-from RecoJets.JetProducers.ak4GenJets_cfi import ak4GenJets
-process.ak4GenJetsCustom = ak4GenJets.clone(
-    src = genJetInputParticleCollection,
-    rParam = cms.double(0.4),
-    jetAlgorithm = cms.string("AntiKt")
-)
-genJetCollection = "ak4GenJetsCustom"
+genJetCollection = "slimmedGenJets"
 
 # Ghost particle collection used for Hadron-Jet association
 # MUST use proper input particle collection
@@ -430,7 +422,7 @@ process.OUT.outputCommands.append("keep *_matchGenBHadron__EX")
 process.OUT.outputCommands.append("keep *_matchGenCHadron__EX")
 process.OUT.outputCommands.append("keep *_matchGenBHadron_*_EX")
 process.OUT.outputCommands.append("keep *_matchGenCHadron_*_EX")
-process.OUT.outputCommands.append("keep *_ak4GenJetsCustom_*_EX")
+#process.OUT.outputCommands.append("keep *_ak4GenJetsCustom_*_EX")
 
 
 
