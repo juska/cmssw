b1bba63dbf7610a9713c27ef2309f38ae80085c3
Joosep Pata <joosep.pata@gmail.com>
2015-09-16 10:50:19 +0200

    longer runtime

--- VHbbAnalysis/Heppy/test/crab/heppy_crab_config.py
+++ VHbbAnalysis/Heppy/test/crab/heppy_crab_config.py
@@ -2,14 +2,16 @@ from WMCore.Configuration import Configuration
 config = Configuration()
 
 config.section_("General")
-config.General.requestName = 'VHBB_HEPPY_V13_002'
-config.General.workArea = 'crab_projects_V13_002'
+config.General.requestName = 'VHBB_HEPPY_V13_003'
+config.General.workArea = 'crab_projects_V13_003'
 config.General.transferLogs=True
 
 config.section_("JobType")
 config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = 'heppy_crab_fake_pset.py'
 config.JobType.scriptExe = 'heppy_crab_script.sh'
+config.JobType.maxJobRuntimeMin = 2800
+
 import os
 os.system("tar czf python.tar.gz --dereference --directory $CMSSW_BASE python")
 config.JobType.inputFiles = ['heppy_config.py',
@@ -37,11 +39,11 @@ config.section_("Data")
 config.Data.inputDataset = '/ZH_HToBB_ZToLL_M125_13TeV_amcatnloFXFX_madspin_pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM'
 config.Data.inputDBS = 'global'
 config.Data.splitting = 'FileBased'
-config.Data.unitsPerJob = 3
+config.Data.unitsPerJob = 2
 #config.Data.totalUnits = 1
-config.Data.outLFNDirBase = '/store/user/jpata/VHBBHeppyV13_test_Sep9_jec_jer/'
+config.Data.outLFNDirBase = '/store/user/jpata/VHBBHeppyV13_test_Sep11/'
 config.Data.publication = True
-config.Data.publishDataName = 'VHBBHeppyV13_test_Sep9_jec_jer'
+config.Data.publishDataName = 'VHBBHeppyV13_test_Sep11'
 
 config.section_("Site")
 config.Site.storageSite = "T2_EE_Estonia"
