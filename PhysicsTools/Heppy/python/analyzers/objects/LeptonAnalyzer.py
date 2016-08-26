e69a17999acc1a5307875e28fb48417402c0b83e
Christian Veelken <christian.veelken@cern.ch>
2016-08-26 10:08:10 +0300

    make gen. matching work for taus (taus are not stable particles and need to be handled differently from electrons and muons)

--- PhysicsTools/Heppy/python/analyzers/objects/LeptonAnalyzer.py
+++ PhysicsTools/Heppy/python/analyzers/objects/LeptonAnalyzer.py
@@ -632,7 +632,7 @@ class LeptonAnalyzer( Analyzer ):
                                        deltaRMax = 1.2, filter_func = plausible)
         for lep in leps:
             gen = match[lep]
-            lep.mcMatchId  = (gen.sourceId if gen != None else  0)
+            lep.mcMatchId  = (gen.sourceId if gen != None and hasattr(gen, "sourceId") else  0)
             lep.mcMatchTau = (gen in event.gentauleps if gen else -99)
             lep.mcLep=gen
 
