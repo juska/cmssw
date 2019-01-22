0e218d9ddcf832f557a18ae3861558f36861d49b
Joosep Pata <joosep.pata@cern.ch>
2019-01-22 23:22:07 +0100

    condor submission

--- Validation/RecoParticleFlow/README.md
+++ Validation/RecoParticleFlow/README.md
@@ -1,6 +1,13 @@
 
 # Quickstart
 
-1. Set up using `test/setup_cmssw.sh`.
-2. Run the RECO and DQM sequences using `test/run_relval.sh`
+1. Set up using `source test/setup_cmssw.sh`.
+2. Make a temporary directory in e.g. `$CMSSW_BASE/src/Validation/RecoParticleFlow/tmp`
+2. Run the RECO sequence using `$CMSSW_BASE/src/Validation/RecoParticleFlow/test/run_relval.sh QCD reco $NJOB` where $NJOB is an integer in [0,249] that is used to select the events for this job, assuming 200 events per job and a RelVal dataset of 50k events
+2. Run the DQM sequence using `test/run_relval.sh QCD dqm 0` 
 3. Produce the plots using `test/compare.py`
+
+# Running on condor
+The reco sequence takes about 10-15 minutes / 100 events, so one would need 125 CPU-hours to run the full 50k events. We have prepared condor scripts to facilitate this on lxbatch.
+Go to a temporary working directory e.g. `$CMSSW_BASE/src/Validation/RecoParticleFlow/tmp`, create a log directory with `mkdir log` and submit the condor jobs using
+`condor_submit ${CMSSW_BASE}/src/Validation/RecoParticleFlow/test/condor_sub.jdl`. Once the jobs are done, the DQM sequence is able to use all step3 EDM files in a single job.
--- Validation/RecoParticleFlow/test/run_relval.sh
+++ Validation/RecoParticleFlow/test/run_relval.sh
@@ -4,11 +4,19 @@
 #https://twiki.cern.ch/twiki/bin/view/CMSPublic/SWGuideCmsDriver
 #https://twiki.cern.ch/twiki/bin/view/CMSPublic/WorkBookDataFormats
 
+env
+
 #abort on error
 set -e
 
+
+#number of events to process per job
+#passed through condor, but here is a default value
+if [ -z "$PERJOB" ]; then
+    PERJOB=200
+fi
+
 #set default conditions
-PERJOB=100
 CONDITIONS=auto:phase1_2017_realistic
 ERA=Run2_2017,run2_nanoAOD_94XMiniAODv1
 NTHREADS=1
@@ -19,14 +27,18 @@ if [ "$#" -ne 3 ]; then
     exit 0
 fi
 
-#index of the job is used to keep track of how many events to skip in the reco step
+#index of the job is used to keep track of which events / files to process in the reco step
 NJOB=$3
 
 #set CMSSW environment and go to condor work dir
 LAUNCHDIR=`pwd`
 source /cvmfs/cms.cern.ch/cmsset_default.sh
-cd /data/jpata/pf-integration/CMSSW_9_4_11/src/Validation/RecoParticleFlow
+
+#this environment variable comes from the condor submit script
+cd $CMSSW_BASE
 eval `scram runtime -sh`
+
+#if the _CONDOR_SCRATCH_DIR is not defined, we are not inside a condor batch job
 if [ -z "$_CONDOR_SCRATCH_DIR" ]; then
     cd $LAUNCHDIR
 else
@@ -37,18 +49,20 @@ fi
 if [ "$1" == "TTbar" ]; then
     INPUT_FILE=root://cms-xrd-global.cern.ch///store/relval/CMSSW_9_4_11_cand2/RelValTTbar_13/GEN-SIM-DIGI-RAW/94X_mc2017_realistic_v15-v1/10000/FA0F368D-D4D2-E811-A159-0025905B8600.root
     NAME=TTbar
+    echo "TTbar sample needs to be implemented"
+    exit 1
 elif [ "$1" == "QCD" ]; then
-    INPUT_FILE=root://cms-xrd-global.cern.ch///store/relval/CMSSW_9_4_11_cand2/RelValQCD_FlatPt_15_3000HS_13/GEN-SIM-DIGI-RAW/PU25ns_94X_mc2017_realistic_v15-v1/10000/F2FEBFDF-69D4-E811-ADF1-0CC47A4C8F0C.root 
+    INPUT_FILELIST=${CMSSW_BASE}/src/Validation/RecoParticleFlow/test/filelist_QCD.txt
     NAME=QCD
 elif [ "$1" == "ZMM" ]; then
     INPUT_FILE=root://cms-xrd-global.cern.ch///store/relval/CMSSW_9_4_11_cand2/RelValZMM_13/GEN-SIM-DIGI-RAW/PU25ns_94X_mc2017_realistic_v15-v1/10000/E81E507D-5DD4-E811-9512-0025905A497A.root 
     NAME=ZMM
+    echo "ZMM sample needs to be implemented"
+    exit 1
 else
     echo "Argument 1 must be [TTbar|QCD|ZMM] but was $1"
     exit 1
 fi
-
-INPUT_FILE=file:///data/jpata/pf-integration/CMSSW_9_4_11/src/Validation/RecoParticleFlow/E403B568-BAEB-E811-BB01-0CC47A4D767E.root
 
 ##Which step to do
 if [ "$2" == "reco" ]; then
@@ -83,7 +97,7 @@ if [ $STEP == "RECO" ]; then
     #Run the actual CMS reco with particle flow.
     #On lxplus, this step takes about 15 minutes / 1000 events
     echo "Running step RECO" 
-    cmsDriver.py step3  --customise_commands="process.source.skipEvents=cms.untracked.uint32($SKIPEVENTS)" --runUnscheduled  --conditions $CONDITIONS -s RAW2DIGI,L1Reco,RECO,RECOSIM,EI,PAT --datatier RECOSIM,AODSIM,MINIAODSIM --nThreads $NTHREADS -n $PERJOB --era $ERA --eventcontent RECOSIM,AODSIM,MINIAODSIM --filein file:$INPUT_FILE --fileout file:step3.root > step3.log  2>&1
+    cmsDriver.py step3  --customise_commands="process.source.skipEvents=cms.untracked.uint32($SKIPEVENTS)" --runUnscheduled  --conditions $CONDITIONS -s RAW2DIGI,L1Reco,RECO,RECOSIM,EI,PAT --datatier RECOSIM,AODSIM,MINIAODSIM --nThreads $NTHREADS -n $PERJOB --era $ERA --eventcontent RECOSIM,AODSIM,MINIAODSIM --filein filelist:$INPUT_FILELIST --fileout file:step3.root | tee step3.log  2>&1
    
     #NanoAOD
     #On lxplus, this step takes about 1 minute / 1000 events
@@ -100,13 +114,15 @@ elif [ $STEP == "DQM" ]; then
 
     #Run the DQM sequences (PF DQM only)
     #override the filenames here as cmsDriver does not allow multiple input files and there is no easy way to merge EDM files
-    cmsDriver.py step5 --customise_commands="process.source.fileNames=cms.untracked.vstring($STEP3FNS)" --conditions $CONDITIONS -s DQM:@pfDQM --datatier DQMIO --nThreads $NTHREADS --era $ERA --eventcontent DQM --filein step3*.root --fileout file:step5.root > step5.log 2>&1
+    cmsDriver.py step5 --customise_commands="process.source.fileNames=cms.untracked.vstring($STEP3FNS)" --conditions $CONDITIONS -s DQM:@pfDQM --datatier DQMIO --nThreads $NTHREADS --era $ERA --eventcontent DQM --filein step3*.root --fileout file:step5.root | tee step5.log 2>&1
 
     #Harvesting converts the histograms stored in TTrees to be stored in folders by run etc
-    cmsDriver.py step6 --conditions $CONDITIONS -s HARVESTING:@pfDQM --era $ERA --filetype DQM --filein file:step5.root --fileout file:step6.root > step6.log 2>&1
+    cmsDriver.py step6 --conditions $CONDITIONS -s HARVESTING:@pfDQM --era $ERA --filetype DQM --filein file:step5.root --fileout file:step6.root | tee step6.log 2>&1
 fi
 
 echo "Exit code was $?"
 tail -n3 *.log
 
 cd ..
+
+find . -name "*"
